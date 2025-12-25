9bb6c1856e322134f43521893340465ac386689d
rafael2026baires <jrazar@hotmail.com>
2025-12-25 17:07:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,17 @@ console.log('>>> startPersistWorker() ejecutado <<<');
 // HTTP server (sirve para health y para "upgrade" a WebSocket)
 const server = http.createServer((req, res) => {
 
+  // === CORS ===
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.writeHead(204);
+    res.end();
+    return;
+  }    
+
   if (req.url === '/health') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({
